c0d59f1e4edf0c25c98369b2c06e2962f13725c0
Alex Ruddick <alex@ruddick.tech>
2024-08-05 12:06:14 -0500

    Bump version to 0.5.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as in_file:
 
 setup(
     name='sartorius',
-    version="0.5.0",
+    version="0.5.1",
     description='Python driver for Sartorius and Minebea Intec scales.',
     long_description=long_description,
     long_description_content_type='text/markdown',
